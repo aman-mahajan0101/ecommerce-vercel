482037be3b6c1a161e4c273b3b7738ce62416f54
Aman Mahajan <amanmahajan987@gmail.com>
2022-04-16 15:44:16 +0530

    new

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.get("/", (req, res) => {
 const productRoutes = require("./routes/productRoutes");
 const authRoutes = require("./routes/authRoutes");
 const cartRoutes = require("./routes/cartRoutes");
-const payment = require("./routes/payment");
+// const payment = require("./routes/payment");
 
 //APIs
 const cartAPI = require("./routes/apis/cartAPI");
@@ -100,7 +100,7 @@ app.use("/products", productRoutes);
 app.use(authRoutes);
 app.use(cartRoutes);
 app.use(cartAPI);
-app.use(payment);
+// app.use(payment);
 app.use(likeProductApi);
 app.use(filterProductsApi);
 app.use(favicon(path.join(__dirname + "/favicon.png")));
